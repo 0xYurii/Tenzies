9ca2be2d8d054d3527437d56185202cfae7a6829
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-08-29 22:02:33 +0100

    feat: update the font using

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import { useState } from "react"
 import Die from "./Die"
 
-
+    /**
+     * Challenge: Create a `Roll Dice` button that will re-roll
+     * all 10 dice
+     * 
+     * Clicking the button should generate a new array of numbers
+     * and set the `dice` state to that new array (thus re-rendering
+     * the array to the page)
+     */
 
 export default function App() {
 
