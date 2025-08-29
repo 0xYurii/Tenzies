96f57cd0d447eb6ade2bd221f1f5332a1c8d7a22
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-08-29 22:13:02 +0100

    feat: adding Roll Dice button

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,30 @@
 import { useState } from "react"
 import Die from "./Die"
 
-    /**
-     * Challenge: Create a `Roll Dice` button that will re-roll
-     * all 10 dice
-     * 
-     * Clicking the button should generate a new array of numbers
-     * and set the `dice` state to that new array (thus re-rendering
-     * the array to the page)
-     */
-
 export default function App() {
-
-  function generateAllNewDice(){
-    return new Array(10)
-    .fill(0)
-    .map(()=>Math.ceil(Math.random() * 6))
-  }
-
-  const [randomArray,setRandomArray]=useState(generateAllNewDice())
-
-
-  const diceElements=randomArray.map(num=><Die value={num}/>)
-  return (
-  <main>
-    <section>
-      {diceElements}
-    </section>
-  </main>
-  )
+    
+    const [dice, setDice] = useState(generateAllNewDice())
+    
+    function generateAllNewDice() {
+        return new Array(10)
+            .fill(0)
+            .map(() => Math.ceil(Math.random() * 6))
+    }
+    
+    function rollDice() {
+        setDice(generateAllNewDice())
+    }
+    
+    const diceElements = dice.map(num => <Die value={num} />)
+    
+    return (
+        <main>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+            
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
+            
+        </main>
+    )
 }
--- src/Die.jsx
+++ src/Die.jsx
@@ -1,9 +1,5 @@
-
-
-export default function Die(props){
-    return(
-        <>
-        <button> {props.value} </button>
-        </>
+export default function Die(props) {
+    return (
+        <button>{props.value}</button>
     )
 }
